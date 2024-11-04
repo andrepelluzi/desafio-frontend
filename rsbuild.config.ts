7f0f773f2cb977b15d2bf657f709a8433f64350e
Andre Pelluzi <andre.pelluzi@gmail.com>
2024-11-04 19:43:02 -0300

    feat: improve seo and acessibility

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
 
 export default defineConfig({
   plugins: [pluginReact()],
-});
+  html: {
+    template: 'src/template.html',
+    meta: {
+      title: 'Diretório de Usuários',
+      description:
+        'Busca e visualiza usuários na API do jsonplaceholder.typicode.com'
+    }
+  }
+})
